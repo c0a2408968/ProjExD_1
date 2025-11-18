202864ecc9c3e590e4da3196bf1f9b08d2bafaa4
c0a24089 <c0a2408968@edu.teu.ac.jp>
2025-11-18 17:09:50 +0900

    練習10_2

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,7 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)#練習8
     kk_img = pg.transform.flip(pg.image.load("fig/3.png"), True, False)#練習3
     kk_rct = kk_img.get_rect()#練習10_1
+    kk_rct.center = 300, 200 #練習10_2
     tmr = 0
     while True:
         for event in pg.event.get():
