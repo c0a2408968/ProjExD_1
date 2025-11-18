8c5a08fbbe410bf26f2c66a90434ef96132ccb9a
c0a24089 <c0a2408968@edu.teu.ac.jp>
2025-11-18 18:00:19 +0900

    応用2

--- pygame_image.py
+++ pygame_image.py
@@ -31,13 +31,16 @@ def main():
         
         key_lst = pg.key.get_pressed() #練習10_3
         
+        y = 0
+        z=0
         kk_rct.move_ip(-1,0) #応用１
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(3, 0)
+            y=3
         if key_lst[pg.K_UP]:#応用1_2
-            kk_rct.move_ip(0, -1)
+            z=-1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, 1)
+            z=1
+        kk_rct.move_ip(y,z) #応用2
         screen.blit(kk_img, kk_rct) #練習10_5
         pg.display.update()
         tmr += 1        
